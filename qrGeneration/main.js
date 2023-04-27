6f1d48f8de823ab3bacd06d509879e05225c2f4f
Ledatu <zavoronok.danila@gmail.com>
2023-04-27 18:43:31 +0300

    Pdf saving of the QRs

--- qrGeneration/main.js
+++ qrGeneration/main.js
@@ -1,4 +1,5 @@
 const QRCode = require("qrcode");
+const { PDFDocument } = require("pdf-lib");
 const path = require("path");
 const fs = require("fs");
 const archiver = require("archiver");
@@ -11,7 +12,6 @@ function zipDirectory(sourceDir, outPath) {
   const archive = archiver("zip", { zlib: { level: 9 } });
   const stream = fs.createWriteStream(outPath);
 
-  console.log("Zipping.");
   return new Promise((resolve, reject) => {
     archive
       .directory(sourceDir, false)
@@ -23,31 +23,57 @@ function zipDirectory(sourceDir, outPath) {
   });
 }
 
+
+async function saveQRPDF(qrDataArray, filePath) {
+  const pdfDoc = await PDFDocument.create();
+  const writeStream = fs.createWriteStream(filePath);
+
+  for (const qrData of qrDataArray) {
+    const buffer = await QRCode.toBuffer(qrData, { errorCorrectionLevel: "H" });
+    const qrImage = await pdfDoc.embedPng(buffer);
+    const page = pdfDoc.addPage([58, 40]);
+    page.drawImage(qrImage, { x: 0, y: 0, width: 40, height: 40 });
+  }
+
+  const pdfBytes = await pdfDoc.save();
+  writeStream.write(pdfBytes);
+  writeStream.end();
+}
+
 async function main() {
   const arch = path.join(__dirname, "files/qrcodes.zip");
   if (fs.existsSync(arch)) {
     fs.rmSync(arch);
   }
 
-  const qrcodes = JSON.parse(fs.readFileSync(path.join(__dirname, "files/qrcodes.json"))).qrcodes;
+  const qrcodes = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "files/qrcodes.json"))
+  ).qrcodes;
   console.log(qrcodes);
   const mainQrDir = path.join(__dirname, "files/qrcodes");
   fs.rmSync(mainQrDir, { recursive: true, force: true });
   fs.mkdirSync(mainQrDir, (err) => {});
 
+  qrs = {};
   for (qr of qrcodes) {
     const params = qr.split(";");
-    const multiplicity = params[3];
-    const uid = params[2];
+    const multiplicity = String(params[3]);
 
-    const qrDir = path.join(mainQrDir, `QR Кратность ${multiplicity}`);
-    if (!fs.existsSync(qrDir)) {
-      fs.mkdirSync(qrDir, (err) => {});
+    if (multiplicity in qrs) {
+      qrs[multiplicity].push(qr);
+    } else {
+      qrs[multiplicity] = [qr];
     }
-    QRCode.toFile(path.join(qrDir, `${uid}.png`), qr);
+  }
+
+  for (let [key, value] of Object.entries(qrs)) {
+    const multPdf = path.join(mainQrDir, `QR Кратность ${key}.pdf`);
+    await saveQRPDF(value, multPdf);
   }
 
   await zipDirectory(mainQrDir, arch);
+  console.log("Zipping complete.");
+
 }
 
 module.exports = {
