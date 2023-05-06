f183c57721f9ab076d90041ec3e887b8605bdde0
Ledatu <zavoronok.danila@gmail.com>
2023-05-06 15:49:59 +0300

    Added roi check for prices updating

--- prices/google_sheets/index.js
+++ prices/google_sheets/index.js
@@ -184,8 +184,9 @@ async function fetchNewPricesAndWriteToJSON(auth, campaign) {
     const rows = res.data.values;
     const data = [];
     rows.forEach((row) => {
+      const roi = Number(row[12].replace("%", "").replace(",", ".").replace(/\s/g, ""));
       const new_price = Number(row[15]);
-      if (!new_price || new_price > 20000 || new_price < 4500) return;
+      if (roi < 0 || !new_price || new_price > 20000 || new_price < 4500) return;
       data.push({ nmId: nmIds[row[0]], price: new_price });
     });
 
