3ef526b1c7a119374c128e107f12785b96351c65
Ledatu <zavoronok.danila@gmail.com>
2023-05-14 16:45:21 +0300

    moar needed

--- analytics/mp_manager/index.js
+++ analytics/mp_manager/index.js
@@ -3,7 +3,13 @@ const scraperController = require("./pageController");
 
 module.exports = async (campaign_id) => {
   const browserInstance = await browserObject.startBrowser();
-  await scraperController(browserInstance, campaign_id).catch(er => browserInstance.close());
-  await browserInstance.close();
-  console.log(campaign_id, "complete!");
+  await scraperController(browserInstance, campaign_id)
+    .then(async (pr) => {
+      await browserInstance.close();
+      console.log(campaign_id, "complete!");
+    })
+    .catch(async (er) => {
+      await browserInstance.close();
+      console.log(campaign_id, "error exited.");
+    });
 };
