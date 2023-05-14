e3affa9edb682ea748b5f7466a4f3cbb9b45daab
Ledatu <zavoronok.danila@gmail.com>
2023-05-14 16:43:41 +0300

    is it needed? idk, but why not

--- analytics/mp_manager/index.js
+++ analytics/mp_manager/index.js
@@ -3,7 +3,7 @@ const scraperController = require("./pageController");
 
 module.exports = async (campaign_id) => {
   const browserInstance = await browserObject.startBrowser();
-  await scraperController(browserInstance, campaign_id);
+  await scraperController(browserInstance, campaign_id).catch(er => browserInstance.close());
   await browserInstance.close();
   console.log(campaign_id, "complete!");
 };
