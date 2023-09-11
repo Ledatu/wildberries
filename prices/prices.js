51ea57a35607f4e18e3c003c7b7eceaa5c5a171c
Ledatu <zavoronok.danila@gmail.com>
2023-09-11 19:28:26 +0300

    fooking include missing

--- prices/prices.js
+++ prices/prices.js
@@ -22,6 +22,7 @@ const {
   getAdvertStatByDayAndWriteToJSONMpManager,
   fetchRksBudgetsAndWriteToJSON,
   calcAvgDrrByArtAndWriteToJSON,
+  generateGeneralMaskFormsAndWriteToJSON,
 } = require("./main");
 const {
   writePrices,
